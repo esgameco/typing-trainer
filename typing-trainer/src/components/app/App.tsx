77de3155384302fae3998f1a17dd436d05ce163d
Chance Vodnoy <esgameco@gmail.com>
2021-07-18 23:02:37 -0700

    Added components directory

---
+++ typing-trainer/src/components/app/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <header>
+                <h1>Head</h1>
+            </header>
+            <main>
+                <p>Test</p>
+            </main>
+        </div>
+    );
+}
+
+export default App;
---
+++ typing-trainer/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+import App from './components/app'
+
+ReactDom.render(
+    <App></App>,
+    document.getElementById('root'),
+);
